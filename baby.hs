c9e892070ac7214718394400c6327b60d23a8320
Koji Ota <afterjnih@gmail.com>
2020-01-30 08:50:31 +0900

    firstTo

--- baby.hs
+++ baby.hs
@@ -296,3 +296,15 @@ needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
 
 encode :: Int -> String -> String
 encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
