4681af2f0ce61ffdfdbbe893e8ca284ed0faf887
Koji Ota <afterjnih@gmail.com>
2020-02-19 02:25:07 +0900

    chap7

--- baby.hs
+++ baby.hs
@@ -457,7 +457,7 @@ treeInsert x (Node a left right)
   | x == a = Node x left right
   | x < a = Node a (treeInsert x left) right
   | x > a = Node a left (treeInsert x right)
-  | otherwise = EmptyTree
+treeInsert _ Node {} = EmptyTree
 
 treeElem :: (Ord a) => a -> Tree a -> Bool
 treeElem x EmptyTree = False
@@ -510,3 +510,7 @@ yesnoIf yesnoVal yesResult noResult =
   if yesno yesnoVal
     then yesResult
     else noResult
+
+instance Functor Tree where
+  fmap f EmptyTree = EmptyTree 
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
